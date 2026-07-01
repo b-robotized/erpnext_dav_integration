7117e4eebaa43f5c9d06c280495390ac3783dd7f
Arun Govind <arun.govind@cloudconverge.io>
2026-07-01 12:01:20 +0300

    Add field of default_share_link_expire_in in dav account

--- erpnext_dav_integration/hooks.py
+++ erpnext_dav_integration/hooks.py
@@ -21,6 +21,7 @@ doc_events = {
 		"before_save": "erpnext_dav_integration.override.contact.sync_contact_to_carddav",
 		"validate": "erpnext_dav_integration.override.contact.validate_contact",
 	}
+    
 }
 scheduler_events = {
 	"cron": {
