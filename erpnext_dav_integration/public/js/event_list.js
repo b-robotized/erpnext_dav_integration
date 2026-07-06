d3b5fb79fb6e256ba2115b580df3b6b6111d74e7
Arun Govind <arun.govind@cloudconverge.io>
2026-07-06 15:08:39 +0300

    Correction

--- erpnext_dav_integration/public/js/event_list.js
+++ erpnext_dav_integration/public/js/event_list.js
@@ -1,43 +1,43 @@
 frappe.listview_settings["Event"] = {
 	onload: function (listview) {
 		// Add a menu item for syncing with DAV Calendar
-		listview.page.add_menu_item(__("Sync with DAV Calendar"), function () {
-			let selected_events = listview.get_checked_items();
+		// listview.page.add_menu_item(__("Sync with DAV Calendar"), function () {
+		// 	let selected_events = listview.get_checked_items();
 
-			if (!selected_events.length) {
-				frappe.msgprint(__("No events selected."));
-				return;
-			}
+		// 	if (!selected_events.length) {
+		// 		frappe.msgprint(__("No events selected."));
+		// 		return;
+		// 	}
 
-			let event_names = selected_events.map((e) => e.name);
+		// 	let event_names = selected_events.map((e) => e.name);
 
-			frappe.call({
-				method: "erpnext_dav_integration.webdav_sync.api.sync_with_dav_calendar",
-				args: {
-					event_names: event_names,
-				},
-				freeze: true,
-				freeze_message: __("Syncing events with DAV Calendar..."),
+		// 	frappe.call({
+		// 		method: "erpnext_dav_integration.webdav_sync.api.sync_with_dav_calendar",
+		// 		args: {
+		// 			event_names: event_names,
+		// 		},
+		// 		freeze: true,
+		// 		freeze_message: __("Syncing events with DAV Calendar..."),
 
-				callback: function (response) {
-					if (response.exc) {
-						console.error("Sync failed:", response.exc);
-						frappe.msgprint(__("Failed to sync events with DAV Calendar."));
-						return;
-					}
+		// 		callback: function (response) {
+		// 			if (response.exc) {
+		// 				console.error("Sync failed:", response.exc);
+		// 				frappe.msgprint(__("Failed to sync events with DAV Calendar."));
+		// 				return;
+		// 			}
 
-					if (response.message) {
-						frappe.msgprint({
-							title: __("Success"),
-							message: __("Selected events synced successfully."),
-							indicator: "green",
-						});
-					} else {
-						frappe.msgprint(__("No response from server."));
-					}
-				},
-			});
-		});
+		// 			if (response.message) {
+		// 				frappe.msgprint({
+		// 					title: __("Success"),
+		// 					message: __("Selected events synced successfully."),
+		// 					indicator: "green",
+		// 				});
+		// 			} else {
+		// 				frappe.msgprint(__("No response from server."));
+		// 			}
+		// 		},
+		// 	});
+		// });
 		// Add a menu item for generating iCalendar file
 
 		// Add a menu item for fetching events from DAV Calendar
